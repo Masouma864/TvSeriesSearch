e19bc38c025c799a0858c645be018a06480b90b3
masouma <masouma2020@gmail.com>
2023-02-09 09:34:49 +0430

    Make tv-series-list components

--- src/components/TvSeries/TvSeriesList.js
+++ src/components/TvSeries/TvSeriesList.js
@@ -1,11 +1,46 @@
 import React from "react";
-import TvSeries from Tvseries;
 
-class TvSeriesList extends Component {
-    state = {  } 
-    render() { 
-        return ();
-    }
-}
+const TvSeriesList = () => (
+    <Container>
+        <Row>
+            <Col className="greeting">
+                <span>Hello, watchers!</span>
+                <span>Tv Series</span>
+            </Col>
+        </Row>
+        <Row>
+            <Col xs={6} className="coll">
+                <p>url</p>
+                <p>type</p>
+                <p>language</p>
+                <p>name</p>
+                <p>status</p>
+            </Col>
+            <Col xs={6} className="coll">
+                <p>url</p>
+                <p>type</p>
+                <p>language</p>
+                <p>name</p>
+                <p>status</p>
+            </Col>
+            <Col xs={6} className="coll">
+                <p>url</p>
+                <p>type</p>
+                <p>language</p>
+                <p>name</p>
+                <p>status</p>
+            </Col>
+            <Col xs={6} className="coll">
+                <p>url</p>
+                <p>type</p>
+                <p>language</p>
+                <p>name</p>
+                <p>status</p>
+            </Col>
+        </Row>
+    </Container>
+
+);
 
 export default TvSeriesList;
+
